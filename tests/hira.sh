c91d9a45ece18c6f2bcec318485e1292f3c8c19a
Elias Le Boudec <contact@eliasleboudec.ch>
2022-06-29 10:10:54 +0200

    new test

--- tests/hira.sh
+++ tests/hira.sh
@@ -1,4 +1,4 @@
-python HIRA.py --down_sample_time 4 --r_obs_min 1.6 --r_obs_max 2 --n_r_obs 1 --n_theta_obs 3 \
- --n_phi_obs 8 --phi_obs_min 0 --phi_obs_max 6.1 --theta_obs_min 0 --theta_obs_max 3.14 --tol 1e-3 \
+python HIRA.py --down_sample_time 6 --r_obs_min 1 --r_obs_max 2 --n_r_obs 1 --n_theta_obs 3 \
+ --n_phi_obs 8 --phi_obs_min 0.1 --phi_obs_max 6.1 --theta_obs_min 0.1 --theta_obs_max 3.14 --tol 1e-5 \
  --n_points 20 --verbose_every 100 --plot True --scale 1e4 --find_center True --max_global_tries 1 \
- --filename ../../../git_ignore/GLOBALEM/hira_v12.txt --order 6
+ --filename ../../../git_ignore/GLOBALEM/hira_v12.txt --order 4 #> data/order-4-v5.txt &
