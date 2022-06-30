a9f4eb0035eeedb9260b9ce31a74c8f7415adddc
Elias Le Boudec <contact@eliasleboudec.ch>
2022-06-30 08:48:28 +0200

    l4 norm

--- tests/hira.sh
+++ tests/hira.sh
@@ -1,4 +1,7 @@
-python HIRA.py --down_sample_time 6 --r_obs_min 1 --r_obs_max 2 --n_r_obs 1 --n_theta_obs 3 \
- --n_phi_obs 8 --phi_obs_min 0.1 --phi_obs_max 6.1 --theta_obs_min 0.1 --theta_obs_max 3.14 --tol 1e-5 \
- --n_points 20 --verbose_every 100 --plot True --scale 1e4 --find_center True --max_global_tries 1 \
- --filename ../../../git_ignore/GLOBALEM/hira_v12.txt --order 4 #> data/order-4-v5.txt &
+for order in 0 1 2 3 4 5
+do
+ python HIRA.py --dt 1e-10 --down_sample_time 4 --r_obs_min 1 --r_obs_max 1.5 --n_r_obs 2 --n_theta_obs 6 \
+    --n_phi_obs 16 --phi_obs_min 0.1 --phi_obs_max 6.18 --theta_obs_min 0.1 --theta_obs_max 3.04 --tol 1e-6 \
+    --n_points 15 --n_tail 10 --verbose_every 100 --plot False --scale 1e6 --find_center True --max_global_tries 1 \
+    --filename ../../../git_ignore/GLOBALEM/hira_v12.txt --order $order > data/order-$order-l4-error-v15.txt &
+done
