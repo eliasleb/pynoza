f57bb9b164f2fc4c6afac449780978ac5117d3c4
Elias Le Boudec <contact@eliasleboudec.ch>
2022-12-07 12:01:57 +0100

    don't skip __init__

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,13 @@ exclude_patterns = []
 
 html_theme = 'sphinxdoc'
 html_static_path = ['_static']
+
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
