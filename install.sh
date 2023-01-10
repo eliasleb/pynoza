59c7339d2e560b3bf0744d7b858d923971ddb7da
Elias Le Boudec <contact@eliasleboudec.ch>
2023-01-10 17:16:55 +0100

    added test line

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ source env/bin/activate
 git clone https://github.com/eliasleb/pynoza
 cd pynoza || exit
 python3 -m pip install --upgrade pip
-pip install --use-feature=in-tree-build .
+pip install .
+python -c "import pynoza"
