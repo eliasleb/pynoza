29436b6286a01ea02a0e0c241c03023c64c876e3
Elias Le Boudec <contact@eliasleboudec.ch>
2023-04-19 17:09:36 +0200

    new comsol data

--- tests/hira.sh
+++ tests/hira.sh
@@ -1,18 +1,18 @@
 #!/bin/zsh
-for order in 0 1 2 3 4 5 6
+for order in 0 1 2 3 4 5
 do
-  for n_points in 40 45 50
+  for n_points in 45
   do
-    for phase_correction in .14
+    for phase_correction in .14 .15 .16
     do
       python HIRA.py --dt 6.4e-12 --down_sample_time 1 \
             --verbose_every 100 --plot False \
-            --filename ../../../git_ignore/ti-hira/ti_hira_v2.txt --center_x -.5 \
+            --filename ../../../git_ignore/ti-hira/ti_hira_v3.txt --center_x -.5 \
             --x1 .8 .9 -.4 .5 --x2 0 0 0 .6 --x3 0 0 .5 0 --find_center True --coeff_derivative 0 \
             --before .6 --phase_correction 0 0 0 $phase_correction --t_max 2.8 \
             --n_points "$n_points" --n_tail "$n_points" \
-            --order $order --scale 1e-8 --shift 2 \
-            > data/v2-ti-hira-order-$order-n_points-$n_points-phase-$phase_correction.txt &
+            --order $order --scale 1e9 --shift 2 \
+            > data/v5-ti-hira-order-$order-n_points-$n_points-phase-$phase_correction.txt &
         done
     done
 done
