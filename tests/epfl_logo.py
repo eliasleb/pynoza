c2e63ec8e3316cc6b78909f95cced45e7c398a60
Elias Le Boudec <contact@eliasleboudec.ch>
2023-05-02 13:53:40 +0200

    far-field

--- tests/epfl_logo.py
+++ tests/epfl_logo.py
@@ -5,15 +5,16 @@ import numpy as np
 import itertools
 from test_EPFL_logo import c_j
 import sympy
+import time
 
 
-def main():
+def main(**args):
     gamma_si = 4e-9
     gamma = 1
     c0 = 299792458. * gamma_si
 
-    order = 20
-    a = 3
+    order = args.get("order", 1)
+    a = 5
 
     length = 2 * a
     length_logo = 171
@@ -36,7 +37,11 @@ def main():
             x2s.append(yi - h0 / 2)
             ws.append(w0)
             hs.append(h0)
-    t = np.linspace(1.4, 3, 1)
+
+    t = np.linspace(19, 20, 1)
+
+    def modulation(x, y):
+        return np.sign(np.sin(2 * np.pi * x / 50 * 3) * np.sin(2 * np.pi * y / 171 * 10))
 
     def current_moment(ax_, ay, az):
         moment = 0
@@ -44,43 +49,65 @@ def main():
             for x_i, y_i, wi, hi in zip(x1s, x2s, ws, hs):
                 moment += c_j(ax_, ay, az, x_i / length_logo * length - d1,
                               y_i / length_logo * length - d2, wi / length_logo * length,
-                              hi / length_logo * length) / gamma_si
+                              hi / length_logo * length) / gamma_si * modulation(x_i, y_i)
         return [0, 0, moment]
 
-    n_pts = 64
-    x1 = np.linspace(-7, 7, n_pts)
-    x2 = np.linspace(-7, 7, n_pts)
+    n_pts = 128
+    x1 = np.linspace(-1.5 * a, 1.5 * a, n_pts)
+    x2 = np.linspace(3 * a, 6 * a, n_pts)
     x3 = np.array((0.,))
     t_sym = sympy.Symbol("t", real=True)
-    h_sym = sympy.erfc((t_sym - gamma) / gamma)
+    # h_sym = (3 * gamma_si * sympy.sqrt(np.pi / 2)) ** -.5 \
+    #         * sympy.exp(-((t_sym-3*gamma)/gamma)**2) * (4*((t_sym-3*gamma)/gamma)**2-2)
+    h_sym = sympy.exp(-(t_sym/sympy.S(gamma))**2)
+    # h_sym = sympy.exp(-(t_sym/gamma)**2)
+    # h_sym_tr = h_sym.subs(t_sym, -t_sym)
 
-    sol = pynoza.solution.Solution(max_order=order,
-                                   wave_speed=c0)
-    sol.recurse()
-    sol.set_moments(current_moment=current_moment)
-    e_field = sol.compute_e_field(x1, x2, x3, t, h_sym, t_sym, verbose=False)
+    sol_causal = pynoza.solution.Solution(max_order=order, wave_speed=c0)
+    sol_causal.recurse()
+    sol_causal.set_moments(current_moment=current_moment)
+    e_field = sol_causal.compute_e_field(x1, x2, x3, t, h_sym, t_sym)
+    # sol_anticausal = pynoza.solution.Solution(max_order=order, wave_speed=c0, causal=False)
+    # sol_anticausal.recurse()
+    # sol_anticausal.set_moments(current_moment=current_moment)
+    # e_field_tr = sol_anticausal.compute_e_field(x1, x2, x3, t, h_sym, t_sym)  # np.flip(e_field, axis=-1)
+
     e_field_norm = np.sum(e_field ** 2, axis=0) ** .5
-    plot_data = np.log10(e_field_norm)
+    # e_field_tr = -np.flip(e_field, axis=-1)
+    # r = np.sqrt(x1[:, np.newaxis]**2 + x2[np.newaxis, :]**2)
+    plot_data = e_field_norm
     # for index, val in np.ndenumerate(plot_data):
     #     r_2 = x1[index[0]] ** 2 + x2[index[1]] ** 2
-    #     if r_2 < (.1 * a) ** 2:
+    #     if r_2 < (.2 * a) ** 2:
     #         plot_data[index] = 0.
     max_data = np.max(plot_data)
     plt.figure(
-        figsize=(6, 6)
+        figsize=(7, 6)
     )
     plt.ion()
+    did_colorbar = False
     for index_t, t_i in enumerate(t):
         plt.gca().clear()
+        # max_data = 1e4
+        # plt.plot(e_field[2, :, 0, 0, index_t], "k-")
+        # plt.plot(e_field_tr[2, :, 0, 0, index_t], "r--")
+        # plt.plot(e_field[2, :, 0, 0, index_t] - e_field_tr[2, :, 0, 0, index_t], "g:")
+        # plt.ylim(-max_data, max_data)
         plt.contourf(
             x1, x2,
             plot_data[:, :, 0, index_t].T,
-            levels=np.linspace(-1, 8, 100),
+            levels=np.linspace(0, max_data, 26),
             cmap="hsv"
         )
         plt.title(f"{t_i=}")
-        plt.waitforbuttonpress()
-    plt.savefig(f"data/logo_order-{order}.pdf")
+        if not did_colorbar:
+            plt.colorbar()
+            did_colorbar = True
+        plt.tight_layout()
+        # plt.waitforbuttonpress()
+        plt.show(block=False)
+        time.sleep(1)
+        plt.savefig(f"data/logo_order-{order}.pdf")
 
 
 if __name__ == "__main__":
@@ -89,5 +116,9 @@ if __name__ == "__main__":
         "text.usetex": False,
         "font.family": "Times"
     })
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--order", metavar="order", type=int, required=True)
+    parsed = parser.parse_args()
+    main(**vars(parsed))
 
