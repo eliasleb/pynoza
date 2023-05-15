96e11b994891e736a3d1aaaa0155175fd8cbbfd9
Elias Le Boudec <contact@eliasleboudec.ch>
2023-05-15 13:33:27 +0200

    init

---
+++ funknoza/field_to_num.m
@@ -0,0 +1,27 @@
+(* ::Package:: *)
+
+(* ::Input::Initialization:: *)
+(* Expect a=..., maxOrder=..., nPoints=... savePath *)
+If[
+Length@$ScriptCommandLine!=4,
+Print["Expect command line arguments a=..., order=..., nPoints=..."];Throw[_];
+,_]
+ToExpression@$ScriptCommandLine[[2]]
+ToExpression@$ScriptCommandLine[[3]]
+ToExpression@$ScriptCommandLine[[4]]
+
+
+(* ::Input::Initialization:: *)
+Efield=Import[ToString@Directory[]<>"/data/field-order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx"];
+norm=Total[Efield^2];
+plotLimitX=6/5a;
+plotLimitY=2a 50/171;
+nPointsX=21;
+nPointsY=Round[nPointsX plotLimitY/plotLimitX];
+xd=Subdivide[-plotLimitX,plotLimitX,nPointsX];
+yd=Subdivide[-plotLimitY,plotLimitY,nPointsY];
+normd=ParallelTable[
+N[norm/.x->xd[[i]]/.y->yd[[j]]],
+{i,1,Length@xd},{j,1,Length@yd}];
+Export[ToString@Directory[]<>"/data/order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx",
+<|"norm"->norm,"x"->xd,"y"->yd,"discrete norm"->normd|>];
