7d49086c121f22948a3a09f20ba99fcd77c012f9
Elias le Boudec <eleboude@jed.jed.cluster>
2023-06-12 15:12:01 +0200

    logo jobs

--- funknoza/funknoza.slurm.sh
+++ funknoza/funknoza.slurm.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -l
 
-for order in {1..39..2}
+for order in {90..90..2}
 do
 	echo "#!/bin/bash -l
 #SBATCH --nodes 1
-#SBATCH --time=72:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=500G
+#SBATCH --mail-user=elias.leboudec@epfl.ch
+#SBATCH --mail-type=ALL
 module purge
 module load mathematica
-wolframscript -script time_domain.m a=4 maxOrder=$order nPoints=31 $PWD/data" > .jobs/order-$order.job
+wolframscript -script field_to_num.m a=4 order=$order nPoints=81" > .jobs/order-$order.job
 	sbatch .jobs/order-$order.job
 done
