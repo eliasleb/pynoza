70f2e2a474634573cb193f8a8f79125be61e659b
Elias le Boudec <eleboude@jed.jed.cluster>
2023-06-12 14:59:42 +0200

    missing assignation

--- funknoza/field_to_num.m
+++ funknoza/field_to_num.m
@@ -12,16 +12,16 @@ ToExpression@$ScriptCommandLine[[4]]
 
 
 (* ::Input::Initialization:: *)
-Efield=Import[ToString@Directory[]<>"/data/field-order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx"];
+Efield=Import[ToString@Directory[]<>"/data/field-v2-order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx"];
 norm=Total[Efield^2];
 plotLimitX=6/5a;
 plotLimitY=2a 50/171;
-nPointsX=21;
+nPointsX=nPoints;
 nPointsY=Round[nPointsX plotLimitY/plotLimitX];
 xd=Subdivide[-plotLimitX,plotLimitX,nPointsX];
 yd=Subdivide[-plotLimitY,plotLimitY,nPointsY];
 normd=ParallelTable[
-N[norm/.x->xd[[i]]/.y->yd[[j]]],
+N[norm/.x->xd[[i]]/.y->yd[[j]],50],
 {i,1,Length@xd},{j,1,Length@yd}];
-Export[ToString@Directory[]<>"/data/order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx",
-<|"norm"->norm,"x"->xd,"y"->yd,"discrete norm"->normd|>];
+Export[ToString@Directory[]<>"/data/v2-order-"<>ToString[order]<>"-a-"<>ToString[a]<>".mx",
+<|"x"->xd,"y"->yd,"discrete norm"->normd|>];
