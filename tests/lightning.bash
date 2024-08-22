4e0dc831d8550d1a0b7d9858887ec3c0368e86f6
Elias Le Boudec <contact@eliasleboudec.ch>
2024-08-22 17:07:17 +0200

    typo

--- tests/lightning.bash
+++ tests/lightning.bash
@@ -7,7 +7,7 @@ do
   do
     for seed in $(seq 0 9)
     do
-      python lightning.py --max_order "$order" --n_points $n_points --n_tail $n_tail --order_scale $scale --seed "$seed" --find_center True > ../../../git_ignore/lightning_inverse/opt_results/max_order_"${order}"_scale_"${scale}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}".txt &
+      python lightning.py --max_order "$order" --n_points $n_points --n_tail $n_tail --order_scale $scale --seed "$seed" --find_center True > ../../git_ignore/lightning_inverse/opt_results/max_order_"${order}"_scale_"${scale}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}".txt &
     done
   done
 done
