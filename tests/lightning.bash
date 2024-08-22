6cb9110df3a3f201df92b54b769a973059c4a3a2
Elias Le Boudec <contact@eliasleboudec.ch>
2024-08-22 17:06:45 +0200

    typo

--- tests/lightning.bash
+++ tests/lightning.bash
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
 scale=1e6
 for n_points in 100
 do
   n_tail=10
-  for order in {0..16..2}
+  for order in $(seq 0 16 2)
   do
-    for seed in {0..0}
+    for seed in $(seq 0 9)
     do
       python lightning.py --max_order "$order" --n_points $n_points --n_tail $n_tail --order_scale $scale --seed "$seed" --find_center True > ../../../git_ignore/lightning_inverse/opt_results/max_order_"${order}"_scale_"${scale}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}".txt &
     done
