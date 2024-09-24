b52c033697af302583236b48bd88c7a80a4e3566
Elias Le Boudec <contact@eliasleboudec.ch>
2024-09-24 17:14:47 +0200

    sweep

--- tests/lightning.sh
+++ tests/lightning.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-n_points=100
-n_tail=10
-for order in $(seq 0 2 16)
+n_points=20
+n_tail=2
+for order in $(seq 0 2 8)
 do
   for seed in $(seq 0 9)
   do
     python lightning.py --max_order "$order" --n_points $n_points --n_tail $n_tail --seed "$seed" --find_center True \
-    > ../../git_ignore/lightning_inverse/opt_results/max_order_"${order}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}".txt &
+    > ../../git_ignore/lightning_inverse/opt_results/\
+    var_max_order_"${order}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}".txt &
+  done
 done
