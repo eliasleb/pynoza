311e71351bb81c9c40d04092ff796d372eb4be82
Elias Le Boudec <contact@eliasleboudec.ch>
2024-10-15 15:04:47 +0200

    path

--- applications/lightning/lightning.sh
+++ applications/lightning/lightning.sh
@@ -10,7 +10,7 @@ do
     do
       python lightning.py --max_order "$order" --n_points $n_points --n_tail $n_tail --seed "$seed" \
       --noise_level $noise_level --case $data_case --scale 1e7 \
-      > ../../git_ignore/lightning_inverse/opt_results/v7_max_order_"${order}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}"_noise_level_"${noise_level}"_case_$data_case.txt &
+      > ../../../git_ignore/lightning_inverse/opt_results/v7_max_order_"${order}"_n_points_"${n_points}"_n_tail_"${n_tail}"_seed_"${seed}"_noise_level_"${noise_level}"_case_$data_case.txt &
     done
   done
 done
